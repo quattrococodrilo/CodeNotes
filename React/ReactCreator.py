bc58c9678df48edf200d1c1afb20f8c106bdb98e
Luis Fernando Cruz Carrillo <quattrococodrilo@gmail.com>
2020-10-18 20:20:04 -0500

    OK

--- React/ReactCreator.py
+++ React/ReactCreator.py
@@ -1,61 +1,118 @@
 #!/usr/bin/python3
 
+import argparse
 import os
 import subprocess
 from urllib import request
 
 
-def reaction():
-    PROJECT = input("Project name: ")
+class Reactor:
+    def __init__(self,
+                 path_directory,
+                 verbose=False,
+                 no_create_files=False,
+                 no_install_npm=False):
 
-    SRC = os.path.join(PROJECT, "src")
-    COMPONENTS = os.path.join(SRC, "components")
-    INDEX_JS = os.path.join(SRC, "index.js")
+        self._verbose = verbose
+        self._no_install_npm = no_install_npm
+        self._no_create_files = no_create_files
+        self._project = path_directory
+        self._src = os.path.join(self._project, "src")
+        self._components = os.path.join(self._src, "components")
+        self._public = os.path.join(self._project, "public")
+        if self._project:
+            self.reaction()
+        else:
+            print("Enter a path and name project (my/project/path/name).")
 
-    PUBLIC = os.path.join(PROJECT, "public")
-    INDEX_HTML = os.path.join(PUBLIC, "index.html")
+    def files(self):
+        """ Return a list of files to be created.
+        Each item must be: {'path': 'path/to/file', 'url': 'url/to/download/file'}  """
 
-    # Root project
-    not os.path.isdir(PROJECT) and os.makedirs(PROJECT)
-    # os.chdir(PROJECT)
+        return [
+            {
+                "path": os.path.join(self._src, "index.js"),
+                "url":  "https://raw.githubusercontent.com/quattrococodrilo/CodeNotes/main/React/Template/src/index.js"
+            },
+            {
+                "path": os.path.join(self._public, "index.html"),
+                "url": "https://raw.githubusercontent.com/quattrococodrilo/CodeNotes/main/React/Template/public/index.html"
+            },
+            {
+                "path": os.path.join(self._project, ".babelrc"),
+                "url": "https://github.com/quattrococodrilo/CodeNotes/blob/main/React/Template/.babelrc"
+            }
+        ]
 
-    not os.path.isdir(SRC) and os.makedirs(SRC)
-    not os.path.isdir(COMPONENTS) and os.makedirs(COMPONENTS)
-    get_data_and_write(
-        INDEX_JS,
-        "https://raw.githubusercontent.com/quattrococodrilo/CodeNotes/main/React/Template/src/index.js"
-    )
+    def npm_modules(self):
+        """ NPM Modules to be installed. """
+        return [
+            "npm install react react-dom",
+            "npm install @babel/core babel-loader @babel/preset-env @babel/preset-react --save-dev",
+        ]
 
-    not os.path.isdir(PUBLIC) and os.makedirs(PUBLIC)
-    get_data_and_write(
-        INDEX_HTML,
-        "https://raw.githubusercontent.com/quattrococodrilo/CodeNotes/main/React/Template/public/index.html"
-    )
+    def reaction(self):
+        """ Creates a structure project, files and install NPM packages. """
+        files_to_create = self.files()
+        not os.path.isdir(self._public) and os.makedirs(self._public)
+        not os.path.isdir(self._project) and os.makedirs(self._project)
+        not os.path.isdir(self._src) and os.makedirs(self._src)
+        not os.path.isdir(self._components) and os.makedirs(self._components)
 
-    NPM_EXIST = subprocess.run(
-        ["which", "npm"],
-        capture_output=True
-    ).stdout.decode(encoding="UTF-8")
+        if not self._no_create_files:
+            for f in files_to_create:
+                self.get_data_and_write(f["path"], f["url"])
+                if self._verbose:
+                    print(f["path"])
+                    print("------------------------------")
 
-    if NPM_EXIST:
-        os.chdir(PROJECT)
-        os.system("npm init -y")
-        os.system("npm install react react-dom")
+        if not self._no_install_npm:
+            NPM_EXIST = subprocess.run(
+                ["which", "npm"],
+                capture_output=True
+            ).stdout.decode(encoding="UTF-8")
 
-        with open(".gitignore", "w") as f:
-            f.write("node_modules/\nnode_modules/*")
-        get_data_and_write(
-            "webpack.config.js"
-        )
+            if NPM_EXIST:
+                print("\n---------- NPM MODULES ----------")
+                modules = self.npm_modules()
+                os.chdir(self._project)
+                os.system("npm init -y")
+                for module in modules:
+                    os.system(module)
+            else:
+                print("NPM not found.")
 
+    def get_data_and_write(self, file_path, url):
+        """Downloads a file from url, and save it
+        on a directory.
 
-def get_data_and_write(file_path, url):
-    """ Get data from url and writhe it in a file. """
-    with open(file_path, "w") as f:
-        req = request.Request(url)
-        with request.urlopen(req) as response:
-            f.write(response.read().decode(encoding="UTF-8"))
+        Args:
+            - file_path (string): place and name to save the file.
+            - url (string): URL to download the file.
+        """
+
+        with open(file_path, "w") as f:
+            req = request.Request(url)
+            with request.urlopen(req) as response:
+                f.write(response.read().decode(encoding="UTF-8"))
 
 
 if __name__ == "__main__":
-    reaction()
+    parser = argparse.ArgumentParser(
+        description="Creates a React project."
+    )
+    parser.add_argument("-p", "--path", required=True,
+                        help="path/to/directory")
+    parser.add_argument("-nm", "--no_install_modules", action="store_true",
+                        help="no install modules")
+    parser.add_argument("-nf", "--no_create_files", action="store_true",
+                        help="no create files")
+    parser.add_argument("-v", "--verbose", action="store_true",
+                        help="show output files creation")
+    args = parser.parse_args()
+    Reactor(
+        path_directory=args.path,
+        verbose=args.verbose,
+        no_create_files=args.no_create_files,
+        no_install_npm=args.no_install_modules
+    )
